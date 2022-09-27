48783003941e9f1c4873c406362c9dad71eb752a
trinhqtkd1994 <trinhqtkd1994@gmail.com>
2022-09-27 11:14:51 +0700

    Hello Kotlin

---
+++ app/src/main/java/com/example/hellokotlin/Activity/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.hellokotlin.Activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.SeekBar
+import com.example.hellokotlin.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+    fun autoSeekbar(){
+
+    }
+}
---
+++ app/src/main/java/com/example/hellokotlin/DataBinding/SplashDataBinding.kt
@@ -0,0 +1,4 @@
+package com.example.hellokotlin.DataBinding
+
+class SplashDataBinding {
+}
